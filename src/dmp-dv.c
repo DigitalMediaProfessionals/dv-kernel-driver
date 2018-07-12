735c5357641868ea83b16be30510cf869c3106fd
LYU, Zong Hong <zonghong.lyu@dmprof.com>
2018-07-12 11:06:26 +0900

    Add user ioctl interface header.

--- src/dmp-dv.c
+++ src/dmp-dv.c
@@ -3,19 +3,15 @@
  *
  *  Copyright (C) 2018  Digital Media Professionals Inc.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #define DRM_DEV_NAME "dmp-dv"
@@ -73,7 +69,7 @@ static const char *subdev_name[2] = { "conv", "fc" };
 #include <linux/of_irq.h>
 #endif
 
-#define DMP_IOC_MAJOR 0x82
+#include "../uapi/dmp-dv.h"
 
 #define REG_IO_ADDR(DV, OF) ((void __iomem *)(DV->bar_logical) + OF)
 
---
+++ uapi/dmp-dv.h
@@ -0,0 +1,100 @@
+/*
+ *  DV700 kernel driver user interface
+ *
+ *  Copyright (C) 2018  Digital Media Professionals Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _UAPI_LINUX_DMP_DV_H
+#define _UAPI_LINUX_DMP_DV_H
+
+#include <linux/ioctl.h>
+#include <linux/types.h>
+
+/**
+ * struct dmp_dv_offset_info - entry of offset filling information array.
+ * @buf_index:		Index of the buffer to use.
+ * @offset:		Bytes offset from the beginning of cmd data.
+ */
+struct dmp_dv_offset_info {
+	__u32 buf_index;
+	__u32 offset;
+};
+
+/**
+ * struct dmp_dv_cmd - cmd data passed from userspace to append to cmd buffer.
+ * @cmd_size:		cmd size in bytes.
+ * @buf_num:		Number of buffers used in this cmd.
+ * @offset_num:		Number of offsets needed to be filled by the kernel.
+ * @cmd_pointer:	Pointer to the cmd data.
+ * @buf_pointer:	Pointer to the used buffers list array. Actual type is
+ *			pointer to fd array <int*>.
+ * @offset_pointer:	Pointer to the offsets filling information array. Actual
+ *			type is <struct dmp_dv_offset_info array*>.
+ */
+struct dmp_dv_cmd {
+	__u32 cmd_size;
+	__u32 buf_num;
+	__u32 offset_num;
+	__u32 reserved0; /* align to 64bits */
+	__u64 cmd_pointer;
+	__u64 buf_pointer;
+	__u64 offset_pointer;
+};
+
+#define DMP_DV_IOC_MAGIC		0x82
+
+/**
+ * DOC: DMP_DV_IOC_RESET - Reset command buffer.
+ *
+ * Reset the command buffer and clear all previously appended commands.
+ */
+#define DMP_DV_IOC_RESET		_IO(DMP_DV_IOC_MAGIC, 0)
+
+/**
+ * DOC: DMP_DV_IOC_APPEND_CMD - Append command(s) to the command buffer.
+ *
+ * Append one (or more) command(s) to the end of command buffer.
+ * Takes a dmp_dv_cmd struct and finds out all dma addresses for buffers used
+ * by the command(s). The buffers should be fds allocated by the ION device.
+ *
+ * If there is not enough memory for the command buffer, returns -ENOMEM.
+ */
+#define DMP_DV_IOC_APPEND_CMD		_IOW(DMP_DV_IOC_MAGIC, 1, \
+						struct dmp_dv_cmd)
+
+/**
+ * DOC: DMP_DV_IOC_RUN - Run all commands in the command buffer.
+ *
+ * Run all commands in the current command buffer. Will acquire the HW lock;
+ * if the HW lock is not immediately available then will block in the kernel
+ * until it get the lock.
+ */
+#define DMP_DV_IOC_RUN			_IO(DMP_DV_IOC_MAGIC, 2)
+
+/**
+ * DOC: DMP_DV_IOC_WAIT - Wait for the previously run to finish.
+ *
+ * Wait for the interrupt if this device is currently running.
+ * Ignore if the hardware is not running or if it is running commands by other
+ * owner.
+ */
+#define DMP_DV_IOC_WAIT			_IO(DMP_DV_IOC_MAGIC, 3)
+
+/**
+ * DOC: DMP_DV_IOC_GET_KICK_COUNT - Get finished kick count.
+ *
+ * Get the finished kick count by the HW.
+ */
+#define DMP_DV_IOC_GET_KICK_COUNT	_IOR(DMP_DV_IOC_MAGIC, 4, __u32)
+
+#endif
