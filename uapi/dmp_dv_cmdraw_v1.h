c1757017ff73013b589ad94db4c16895f6c7fdfb
LYU, Zong Hong <zonghong.lyu@dmprof.com>
2019-05-31 13:44:00 +0900

    Fix indentation

--- uapi/dmp_dv_cmdraw_v1.h
+++ uapi/dmp_dv_cmdraw_v1.h
@@ -28,11 +28,11 @@ struct dmp_dv_kcmdraw_conv_v1 {
 	struct dmp_dv_kcmdraw header;  // General structure information
 
 	struct dmp_dv_kbuf u8tofp16_table; // If this table exist (fd >= 0), input buffer is in u8 format
-                                     // and should be converted to fp16 with this table
-  __u16 is_bgr;                 // Should output BGR format
-  __u16 rsvd[3];
-  
-  // The following is the same as v0, so should be able to reuse implementation in kernel
+	                                   // and should be converted to fp16 with this table
+	__u16 is_bgr;                      // Should output BGR format
+	__u16 rsvd[3];
+
+	// The following is the same as v0, so should be able to reuse implementation in kernel
 	struct dmp_dv_kbuf input_buf;      // Input buffer
 	struct dmp_dv_kbuf output_buf;     // Output buffer
 	struct dmp_dv_kbuf eltwise_buf;    // Buffer for elementwise add (0 = UBUF Input Buffer)
